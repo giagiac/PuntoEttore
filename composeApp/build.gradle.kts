df85092268e7a7a4f11568bca4dc04c0243b3390
Giacomo Lavermicocca <giacomo.cervignano@gmail.com>
2025-01-24 13:24:15 +0100

    test action

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -161,8 +161,8 @@ android {
         applicationId = "it.puntoettore.fidelity"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         signingConfig = signingConfigs.getByName("debug")
     }
     buildFeatures {
